091e2be066d38ce5e5d40811485dea703f9fbaf4
gobycasy <gobycasy@gmail.com>
2026-03-16 22:18:19 +0900

    state追加

--- test/src/App.tsx
+++ test/src/App.tsx
@@ -1,8 +1,15 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import { LinkButton } from "./components/LinkButton";
 
 export const App = () => {
   const title: string = "Hello World!";
+  const [num, setNum] = useState(0);
+
+  const increment = () => {
+    setNum(num + 1);
+    console.log(num);
+  };
 
   return (
     <div className="App">
@@ -11,6 +18,10 @@ export const App = () => {
         ボタン
       </Link>
       <LinkButton text="ボタン" link="/test" />
+      {num}回押しました。
+      <button type="button" onClick={increment}>
+        増やす
+      </button>
     </div>
   );
 }
